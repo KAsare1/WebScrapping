1fa940c5eeff527b9034e89235ca0453ada67d21
KAsare1 <asarek977@gmail.com>
2024-01-04 22:51:12 +0000

    l1

---
+++ web_scrapping/web_scrapping/spiders/quotes_spider.py
@@ -0,0 +1,12 @@
+from typing import Any
+import scrapy
+from scrapy.http import Response
+
+class Quotes(scrapy.Spider):
+    name = 'quotes'
+    start_url = ['https://quotes.toscrape.com/']
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        title = response.css('title').extract()
+        yield{'titltext': title}
+        return super().parse(response, **kwargs)
